03f6143842338d46744495d90d0ac413a9c91fd3
daedalusrex <daedalusrex@gmail.com>
2013-02-10 14:33:12 -0500

    Added Client Server stuff

---
+++ Winter Wars/GameStateManagementSample/Code/Game Objects/Mediator_Player_Controls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WWxna.Code.MVC;
+
+namespace WWxna.Code.Game_Objects
+{
+    class Mediator_Player_Controls
+    {
+        private Player p;
+        private Controls c;
+    }
+}
